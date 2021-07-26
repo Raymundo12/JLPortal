486139df94bdf22c78fdfbbf296cb932ad9bea8a
Raymundo12 <raymundo.olverap@gmail.com>
2021-07-26 20:05:23 +0000

    Conecction

--- Crud-JPortal/server.js
+++ Crud-JPortal/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const dotenv=require('dotenv');
+const morgan=require('morgan');
 const app = express();
+
+dotenv.config({path:dotenv.config.env})
+const PORT=process.env.PORT ||8080
+
+app.use(morgan('tiny'));
 
 app.get('/',(req,res)=>{
     res.send("Almost There");
 })
 
-app.listen(3000,()=>{console.log('server is runing on http://localhost:${3000}')});
+app.listen(PORT,()=>{console.log('server is runing on http://localhost:${PORT}')});
 
