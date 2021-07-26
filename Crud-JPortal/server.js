290997cd8ba2b5f559eac6921303caa153f39a88
Raymundo12 <raymundo.olverap@gmail.com>
2021-07-26 20:41:50 +0000

    index.ejs

--- Crud-JPortal/server.js
+++ Crud-JPortal/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dotenv=require('dotenv');
 const morgan=require('morgan');
+const bodyparser=require("body-parser");
+const path = require("path");
+const { resolve } = require('dns');
+
 const app = express();
 
 dotenv.config({path:dotenv.config.env})
@@ -8,8 +12,15 @@ const PORT=process.env.PORT ||8080
 
 app.use(morgan('tiny'));
 
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.set("view engine","ejs")
+
+app.use('/ccc',express.static((path.resolve(__dirname,"assets/css"))))
+app.use('/js',express.static((path.resolve(__dirname,"assets/js"))))
+
 app.get('/',(req,res)=>{
-    res.send("Almost There");
+    res.render('index');
 })
 
 app.listen(PORT,()=>{console.log('server is runing on http://localhost:${PORT}')});
