3c87263b61bc309ef4533fb1aba6dbcef0334989
Raymundo12 <raymundo.olverap@gmail.com>
2021-07-27 21:48:47 +0000

    Specifying mongo url route

--- Crud-JPortal/server/routes/router.js
+++ Crud-JPortal/server/routes/router.js
@@ -15,3 +15,17 @@ route.get('/update-user',(req,res)=>{
 })
 
 module.exports = route
+
+const mongoose = require('mongoose');
+const connectDB = async()=>{
+    try {
+        const con = await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useFindAndModify:false,
+            useCreateIndex:true
+        })
+    } catch (error) {
+        
+    }
+}
