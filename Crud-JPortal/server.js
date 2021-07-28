bac2f892e2f4d54d085661c3fa2790577cfa972d
Raymundo12 <raymundo.olverap@gmail.com>
2021-07-28 00:11:58 +0000

    Hidding URL

--- Crud-JPortal/server.js
+++ Crud-JPortal/server.js
@@ -15,9 +15,9 @@ dotenv.config({path:dotenv.config.env})
 const PORT=process.env.PORT ||8080
 
 app.use(morgan('tiny'));
-
-mongoose.connect('mongodb+srv://RaymundoOlvera:Ray1234@cluster0.rrjgj.mongodb.net/users?retryWrites=true&w=majority',
-{ useUnifiedTopology: true },()=>
+//{useNewUrlParser: true, useUnifiedTopology: true })//for avoid the deprecation Warning at the terminal
+mongoose.connect('mongodb+srv://RaymundoOlvera:Ray1234@cluster0.rrjgj.mongodb.net/users?retryWrites=true&w=majority', 
+{ useUnifiedTopology: true, useNewUrlParser: true },()=>
 console.log('Conenected to the DB'));
 
 
